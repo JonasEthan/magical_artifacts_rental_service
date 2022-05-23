0aebd814effb38b2f0dd92401443b026e96ebc9b
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-05-23 16:00:32 +0200

    Add routes for Artifacts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :artifacts
   # Defines the root path route ("/")
 end
