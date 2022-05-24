860d84f121b50d1b561ae04556723885223d0c69
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-05-24 21:43:50 +0200

    quick fix for the root issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
   resources :artifacts do
     resources :bookings, only: [:create]
   end
